56d536d1d68b4a0bad7310ff39ed3ad78343a570
CrippledTrack <CrippledTrack@gmail.com>
2025-10-16 21:42:14 -0700

    Added a minimal plugin that contains nothing but what is needed to register a plugin to make it very clear on what is needed

---
+++ plugins/minimal_plugin.py
@@ -0,0 +1,27 @@
+"""
+Minimal plugin example - contains only what's necessary to register as a plugin.
+"""
+from PySide6.QtWidgets import QWidget, QLabel
+from GUI.plugins.base import BaseTabPlugin
+
+class MinimalPlugin(BaseTabPlugin):
+    """Minimal plugin with only required components."""
+    
+    tab_name = "Minimal Plugin"
+    tab_description = "A minimal plugin with only required components"
+    supported_platforms = ["Windows", "Linux"]
+    plugin_version = "1.0.0"
+    plugin_author = "Plugin Creator"
+    disabled_by_default = True
+
+    @classmethod
+    def create_widget(cls, parent=None):
+        """Create the widget for this tab."""
+        return MinimalWidget(parent)
+
+
+class MinimalWidget(QWidget):
+    """Minimal widget that is completely blank."""
+    
+    def __init__(self, parent=None):
+        super().__init__(parent)
