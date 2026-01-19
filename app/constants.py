3948b8b965cdd63f9c6c2b45e7f9596479c43375
CrippledTrack <CrippledTrack@Gmail.com>
2026-01-19 02:19:37 -0800

    Minor update to expand qt dependency system on linux

--- app/constants.py
+++ app/constants.py
@@ -12,7 +12,7 @@ import platform
 # =============================================================================
 # Version Information (can be overridden by app_plugins/constants.py)
 # =============================================================================
-VERSION = "4.0.0"
+VERSION = "4.0.1-dev-1"
 VERSION_NAME = "Basic UI Application"
 
 VERSION_INFO = {
